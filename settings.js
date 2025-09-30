7b234581e17effa655c48b93a75bd008546dd812
Owen-Morgan825 <owen.morgan092@gmail.com>
2025-09-30 11:14:38 -0400

    re-upload due to local updates
    Git wasn't being helpful

---
+++ settings.js
@@ -0,0 +1,19 @@
+const settingsElementMap = {
+  toolbar: {
+    file: {
+      showFileMenu: document.getElementById('settings-toolbar-file-show-menu'),
+      openLiveFile: document.getElementById('settings-toolbar-file-open-live')
+    },
+    edit: {
+      showEditMenu: document.getElementById('settings-toolbar-edit-show-menu')
+    },
+    view: {
+      showViewMenu: document.getElementById('settings-toolbar-view-show-menu')
+    }
+  },
+  editor: {
+    tabSetSize: document.getElementById('settings-editor-tab-set-size')
+  }
+}
+
+export { settingsElementMap };
